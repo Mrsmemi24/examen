7e47643b01875f04d1bb46707192fdbe1ffab23c
Mrsmemi24 <20243rd027@utez.edu.mx>
2025-06-06 12:28:20 -0600

    le cambie los nombres jejeje

--- Main.java
+++ Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        RecursosHumanos rh = new RecursosHumanos("Jorge", 150, 3500, "Alto");
-        Produccion prod = new Produccion("Ivan", 152, 7560, "Matutino");
-        Contabilidad cont = new Contabilidad("Pancho", 153, 5000, "Intermedio");
-        SoporteTecnico soporte = new SoporteTecnico("Tizoc", 155, 10000, "Nivel 5");
+        RecursosHumanos rh = new RecursosHumanos("Aldoctor", 150, 3500, "Alto");
+        Produccion prod = new Produccion("Messientocansao", 152, 7560, "Matutino");
+        Contabilidad cont = new Contabilidad("Pachecocarafloja", 153, 5000, "Intermedio");
+        SoporteTecnico soporte = new SoporteTecnico("Dedtector", 155, 10000, "Nivel 5");
 
         rh.mostrarDatos();
         prod.mostrarDatos();
